723b258b55fe095cf202f196ce5114978e74988b
Aleksey Petukhov <ikazman@yandex.ru>
2021-11-06 12:12:38 +0300

    Email tweaks

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -115,11 +115,11 @@ REST_FRAMEWORK = {
 #  подключаем движок filebased.EmailBackend
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 # указываем директорию, в которую будут складываться файлы писем
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
 CACHES = {
